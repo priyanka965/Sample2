88af419ac5c081399fb4c330eaabf8ede63635e3
Priyanka <priya.sudama@gmail.com>
2026-09-23 14:37:05 +0530

    first commit

---
+++ SampleMavenExcution/src/test/java/pac1/CreateContactTest.java
@@ -0,0 +1,18 @@
+package pac1;
+
+import org.testng.annotations.Test;
+
+
+public class CreateContactTest {
+	@Test
+	public void createContactTest() {
+		System.out.println("execute createContactTest");
+	}
+	
+    @Test
+	public void modifyContactTest() {
+		System.out.println("execute modifyContactTest");
+	}
+	
+
+}
---
+++ SampleMavenExcution/src/test/java/pac2/OrgTest.java
@@ -0,0 +1,23 @@
+package pac2;
+
+import org.testng.annotations.Test;
+
+public class OrgTest {
+	
+
+public class CreateOrgTest {
+	@Test
+	public void createContactTest() {
+		System.out.println("execute CreateOrgTest");
+	}
+	
+    @Test
+	public void modifyOrgTest() {
+		System.out.println("execute modifyOrgTest");
+	}
+	
+
+}
+
+
+}
